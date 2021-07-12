b99cf403bff2b75e83560cf8c63d1c70c5d0f0d5
Derek Su <86570562+Dsu79@users.noreply.github.com>
2021-07-12 16:15:50 +0000

    Stuff was changed

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame as pg
+
+pg.init()
+# init starts pygame but in this instance known as pg
+print(pg.display.Info())
+# this yonks the information about display from the libary pygame (pg)
+# making something modular makes it easier to rtead/comprehend
+#
