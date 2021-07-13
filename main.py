b85b8f94646ee66d224dcab369e201478a45bae6
Derek Su <86570562+Dsu79@users.noreply.github.com>
2021-07-13 14:43:54 +0000

    fosh, I mean fish.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame as pg
 
 pg.init()
+p = print
 # init starts pygame but in this instance known as pg
-print(pg.display.Info())
+p(pg.display.Info())
 # "print(pg.display.Info())" this yonks the information about display from the libary pygame (pg)
 # making something modular makes it easier to rtead/comprehend
 #class defines an objects and decides that attrubutes the object may/can have
@@ -10,18 +11,28 @@ print(pg.display.Info())
 # __init__ , used for making classes
 # "A constructor enables you to provide any custom initialization that must be done before any other methods can be called on an instantiated object." (Javascript)
 screen_info = pg.display.Info()
-print(screen_info.current_w, screen_info.current_h)
-size = screen_info.current_w, screen_info.current_h
-fish_image =  pg.image.load('download.html')
-print(size)
+p(screen_info.current_w, screen_info.current_h)
+size = (width,height) =  screen_info.current_w, screen_info.current_h
+#You could also do
+#size = (width,height)
+#width = screen_info,current_w
+#length = screen_info,current_l
+fish_image =  pg.image.load('fish.jpeg')
+p(size)
 screen = pg.display.set_mode(size)
 clock = pg.time.Clock()
+fish_image = pg.transform.smoothscale(fish_image , (80,80) )
+fish_rect = fish_image.get_rect
+fish_rect = ( width // 2, height // 2 )
+
 def main():
   while True:
     clock.tick(60)
     screen.fill((0,0,0))
-    pg.display.flip()
     screen.blit(fish_image, fish_rect)
+    pg.display.flip()
+
 if __name__ == "__main__":
   main()
+#  checks if the language used it python and if so main() can be run
 main()
